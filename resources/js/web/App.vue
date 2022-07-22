d85ec3a258d3a5161e2c2c6f7adb97f006f7a0ab
Fat Tiger <lewis@hypoidea.com>
2022-07-22 17:45:27 +0800

    fix merge mistake

--- resources/js/web/App.vue
+++ resources/js/web/App.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <q-layout
     view="hHh lpR fFf"
     class="bg-grey-1 flex flex-col h-screen justify-between"
@@ -20,24 +19,9 @@
           v-if="!$route.meta.keepAlive"
         />
       </router-view>
+      <pagefooter></pagefooter>
     </q-page-container>
-    <pagefooter></pagefooter>
   </q-layout>
-=======
-    <q-layout view="hHh lpR fFf" class="bg-grey-1 flex flex-col h-screen justify-between">
-        <topmenu></topmenu>
-        <q-page-container>
-            <router-view v-slot="{ Component }">
-                <keep-alive :max="15">
-                    <component :is="Component" :key="$route.name" v-if="$route.meta.keepAlive" />
-                </keep-alive>
-                <component :is="Component" :key="$route.name" v-if="!$route.meta.keepAlive" />
-            </router-view>
-            <pagefooter></pagefooter>
-        </q-page-container>
-
-    </q-layout>
->>>>>>> 96cfd9f6973dbd5b5161ab407ca72ace004287a6
 </template>
 
 <script setup>
