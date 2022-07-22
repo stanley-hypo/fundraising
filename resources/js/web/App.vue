a526f0035bcaf05167b005f1801f6263d602fa88
Fat Tiger <lewis@hypoidea.com>
2022-07-22 17:39:52 +0800

    donation styling

--- resources/js/web/App.vue
+++ resources/js/web/App.vue
@@ -1,19 +1,30 @@
 <template>
-    <q-layout view="hHh lpR fFf" class="bg-grey-1 flex flex-col h-screen justify-between">
-        <topmenu></topmenu>
-        <q-page-container>
-            <router-view v-slot="{ Component }">
-                <keep-alive :max="15">
-                    <component :is="Component" :key="$route.name" v-if="$route.meta.keepAlive" />
-                </keep-alive>
-                <component :is="Component" :key="$route.name" v-if="!$route.meta.keepAlive" />
-            </router-view>
-        </q-page-container>
-        <pagefooter></pagefooter>
-    </q-layout>
+  <q-layout
+    view="hHh lpR fFf"
+    class="bg-grey-1 flex flex-col h-screen justify-between"
+  >
+    <topmenu></topmenu>
+    <q-page-container>
+      <router-view v-slot="{ Component }">
+        <keep-alive :max="15">
+          <component
+            :is="Component"
+            :key="$route.name"
+            v-if="$route.meta.keepAlive"
+          />
+        </keep-alive>
+        <component
+          :is="Component"
+          :key="$route.name"
+          v-if="!$route.meta.keepAlive"
+        />
+      </router-view>
+    </q-page-container>
+    <pagefooter></pagefooter>
+  </q-layout>
 </template>
 
 <script setup>
-import topmenu from "./components/Layout/Topmenu.vue"
-import pagefooter from "./components/Layout/Footer.vue"
+import topmenu from "./components/Layout/Topmenu.vue";
+import pagefooter from "./components/Layout/Footer.vue";
 </script>
--- resources/js/web/view/Donation.vue
+++ resources/js/web/view/Donation.vue
@@ -1,17 +1,67 @@
 <template>
-    <section>
-            hihi
-    </section>
+  <div class="max-w-5xl p-10 bg-white mx-auto my-20 rounded-md drop-shadow-lg">
+    <form class="donationForm">
+      <h5 class="text-center">Donation</h5>
+      <div class="rectangleRadio">
+        <input
+          type="radio"
+          id="oneOffDonation"
+          name="donationType"
+          value="oneOffDonation"
+          v-model="donationType"
+          checked
+        />
+        <label for="oneOffDonation">One-off Donation</label>
+        <input
+          type="radio"
+          id="monthlyDonation"
+          name="donationType"
+          value="monthlyDonation"
+          v-model="donationType"
+        />
+        <label for="monthlyDonation">Monthly Donation</label>
+      </div>
+      <hr />
+      <h5 class="text-center">Donation</h5>
+      <q-input
+        outlined
+        v-model="ph"
+        placeholder="Placeholder"
+        :dense="dense"
+        class="max-w-lg"
+      />
+      <div>
+        <label for="name">Name</label>
+        <input type="text" id="name" placeholder="Enter your name" />
+      </div>
+      <div>
+        <label for="age">Your Age</label>
+        <input type="text" id="age" name="age" placeholder="Enter your age" />
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
-    props:[],
-    name: 'Donation',
-    components: {
-
-    }
-
-}
+  props: [],
+  name: "Donation",
+  components: {},
+  data() {
+    return {
+      donationType: "",
+    };
+  },
+  setup() {
+    return {
+      shape: ref(""),
+      text: ref(""),
+      ph: ref(""),
+      dense: ref(true),
+    };
+  },
+};
 </script>
+
 
