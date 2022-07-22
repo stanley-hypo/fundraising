eeeaf161344f8e6a46eb357e691b9ec817753f8c
Fat Tiger <lewis@hypoidea.com>
2022-07-22 18:45:10 +0800

    donation styling

--- resources/js/web/view/Donation.vue
+++ resources/js/web/view/Donation.vue
@@ -4,42 +4,71 @@
       class="max-w-5xl p-10 bg-white mx-auto my-20 rounded-md drop-shadow-lg"
     >
       <form class="donationForm">
-        <h5 class="text-center">Donation</h5>
-        <div class="rectangleRadio">
-          <input
-            type="radio"
-            id="oneOffDonation"
-            name="donationType"
-            value="oneOffDonation"
-            v-model="donationType"
-            checked
-          />
-          <label for="oneOffDonation">One-off Donation</label>
-          <input
-            type="radio"
-            id="monthlyDonation"
-            name="donationType"
-            value="monthlyDonation"
-            v-model="donationType"
-          />
-          <label for="monthlyDonation">Monthly Donation</label>
+        <h5 class="text-center my-5">Donation</h5>
+        <!-- Donation Type -->
+        <div class="row">
+          <div class="col-3 self-center break-words">
+            <label for="">Donation Type</label>
+          </div>
+          <div class="rectangleRadio col-9">
+            <input
+              type="radio"
+              id="oneOffDonation"
+              name="donationType"
+              value="oneOffDonation"
+              v-model="donationType"
+              checked
+            />
+            <label for="oneOffDonation">One-off Donation</label>
+            <input
+              type="radio"
+              id="monthlyDonation"
+              name="donationType"
+              value="monthlyDonation"
+              v-model="donationType"
+            />
+            <label for="monthlyDonation">Monthly Donation</label>
+          </div>
         </div>
+        <!-- Donation Amount -->
+        <!-- <div class="row">
+          <div class="col-3 self-center break-words">
+            <label for="">Donation Amount</label>
+          </div>
+          <div class="rectangleRadio col-9">
+            <input
+              type="radio"
+              id="oneOffDonation"
+              name="donationType"
+              value="200"
+              v-model="donationType"
+              checked
+            />
+            <label for="oneOffDonation">One-off Donation</label>
+            <input
+              type="radio"
+              id="monthlyDonation"
+              name="donationType"
+              value="monthlyDonation"
+              v-model="donationType"
+            />
+            <label for="monthlyDonation">Monthly Donation</label>
+          </div>
+        </div> -->
         <hr />
-        <h5 class="text-center">Donation</h5>
-        <q-input
-          outlined
-          v-model="ph"
-          placeholder="Placeholder"
-          :dense="dense"
-          class="max-w-lg"
-        />
-        <div>
-          <label for="name">Name</label>
-          <input type="text" id="name" placeholder="Enter your name" />
-        </div>
-        <div>
-          <label for="age">Your Age</label>
-          <input type="text" id="age" name="age" placeholder="Enter your age" />
+        <h5 class="text-center my-5">Donor Information</h5>
+        <div class="row">
+          <div class="col-3 self-center break-words">
+            <label for="">ASDASDasdasdasdnasd,nassadasdasdasdasdsadAS</label>
+          </div>
+          <div class="col-9">
+            <q-input
+              outlined
+              v-model="ph"
+              placeholder="Placeholder"
+              :dense="dense"
+            />
+          </div>
         </div>
       </form>
     </div>
